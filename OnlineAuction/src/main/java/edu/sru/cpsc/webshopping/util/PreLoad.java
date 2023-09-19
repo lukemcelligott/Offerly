32bdef1f7c5e99eba4bf9196b83a5b8d8fc6c114
Douglas M <dsm1015@sru.edu>
2023-09-19 18:59:10 -0400

    #38 Final Docs and SQL Import Spinner

--- OnlineAuction/src/main/java/edu/sru/cpsc/webshopping/util/PreLoad.java
+++ OnlineAuction/src/main/java/edu/sru/cpsc/webshopping/util/PreLoad.java
@@ -22,10 +22,21 @@ import java.util.Map;
 public class PreLoad {
 
   private final CategoryRepository categoryRepository;
+  private static final char[] SPINNER_CHARS = {'|', '/', '-', '\\'};
 
   @Autowired
   public PreLoad(CategoryRepository categoryRepository) {
       this.categoryRepository = categoryRepository;
+  }
+
+  private int countLines(String filePath) throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        int lines = 0;
+        while (br.readLine() != null) {
+            lines++;
+        }
+        return lines;
+    }
   }
 
   /*
@@ -36,34 +47,47 @@ public class PreLoad {
       String csvFilePath = "src/main/resources/static/preload/category-tree.csv";
       Map<String, Category> categoryMap = new HashMap<>();
 
-      try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
-          String line;
-          while ((line = br.readLine()) != null) {
-            // Check for BOM and remove it
-            if (line.startsWith("\uFEFF")) {
-              line = line.substring(1);
-            }
-            String[] values = line.split(",");
+      int totalLines;
+      try {
+          totalLines = countLines(csvFilePath);
+      } catch (IOException e) {
+          e.printStackTrace();
+          return;
+      }
 
-            // Filter out empty values and get the category and its parent
-            List<String> nonEmptyValues = Arrays.stream(values).filter(v -> !v.trim().isEmpty()).collect(Collectors.toList());
-            String categoryName = nonEmptyValues.get(nonEmptyValues.size() - 1);
-            String parentName = nonEmptyValues.size() > 1 ? nonEmptyValues.get(nonEmptyValues.size() - 2) : null;
-
-            Category category = new Category(categoryName);
-
-            // If the category has a parent
-            if (parentName != null) {
-                Category parentCategory = categoryMap.get(parentName);
-                category.setParent(parentCategory);
-            }
-
-            // Persist the category to the database
-            categoryRepository.save(category);
-
-            // Add the category to the map
-            categoryMap.put(categoryName, category);
+      try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath));
+        Spinner spinner = new Spinner()) {
+        
+        String line;
+        int currentLine = 0;
+        while ((line = br.readLine()) != null) {
+          currentLine++;
+          System.out.print("\rSQL Import " + currentLine + " / " + totalLines + " " + spinner.getCurrentSpinnerChar());
+          // Check for BOM and remove it
+          if (line.startsWith("\uFEFF")) {
+            line = line.substring(1);
           }
+          String[] values = line.split(",");
+
+          // Filter out empty values and get the category and its parent
+          List<String> nonEmptyValues = Arrays.stream(values).filter(v -> !v.trim().isEmpty()).collect(Collectors.toList());
+          String categoryName = nonEmptyValues.get(nonEmptyValues.size() - 1);
+          String parentName = nonEmptyValues.size() > 1 ? nonEmptyValues.get(nonEmptyValues.size() - 2) : null;
+
+          Category category = new Category(categoryName);
+
+          // If the category has a parent
+          if (parentName != null) {
+              Category parentCategory = categoryMap.get(parentName);
+              category.setParent(parentCategory);
+          }
+
+          // Persist the category to the database
+          categoryRepository.save(category);
+
+          // Add the category to the map
+          categoryMap.put(categoryName, category);
+        }
       } catch (Exception e) {
           e.printStackTrace();
       }
